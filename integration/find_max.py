58d60ada952f576fc778f48fb69018d4a2138a48
saikiran2908 <158637044+saikiran2908@users.noreply.github.com>
2024-02-14 16:07:12 -0500

    Update find_max.py

--- integration/find_max.py
+++ integration/find_max.py
@@ -8,5 +8,5 @@ def test_c_max():
 	# Expected output
 	expected_output = 8
 	# Check if the function returns the correct output
-	assert maximum(x, y, z) == expected_output, "Output doesn't match the expected value"
+	assert mxValue.maximum(x, y, z) == expected_output, "Output doesn't match the expected value"
 	
