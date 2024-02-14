fa19e1082d62fcaff7dfa0d51eaeae32f7dc60cf
saikiran2908 <158637044+saikiran2908@users.noreply.github.com>
2024-02-14 15:56:40 -0500

    Update check_swap.py

--- integration/check_swap.py
+++ integration/check_swap.py
@@ -1,9 +1,9 @@
 import swap
 
 
-def c_swap():
+def test_c_swap():
 	x = 5
 	y = 10
-	result = swap.swap_no(x, y) == 15
+	result = swap.swap_no(x, y)
 
 	assert result == 15, "Value of x is not correctly swapped"
